fd8a8933501596c87d2c8b25944b0f121efb75e5
Rajan <dos.rajanverma@gmail.com>
2026-06-15 16:24:26 +0530

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,6 +30,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <meta name="google-site-verification" content="jaq_dDcIxfu1kr5mb2spImgR2KHWCw3Cy9470acoYL4" />
+      </head>
       <body className="min-h-full flex flex-col">
         <Navbar />
         <main className="flex-1">{children}</main>
